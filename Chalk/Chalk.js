8c428bf7f4aa8da7d8ccf41ff3a156c4607f5cb1
troyjwingo <31901732+troyjwingo@users.noreply.github.com>
2017-10-31 22:40:54 -0500

    This is Chalk npm node practice...

---
+++ Chalk/Chalk.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+var msg = 'Hello World';
+
+console.log(chalk.red('Hi Im Troy,'),chalk.green('I woke up this morning'),chalk.blue('Then I went back to bed.'),chalk.magenta('THE END.'));
