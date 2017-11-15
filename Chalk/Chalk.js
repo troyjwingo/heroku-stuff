aa48b70bed8d9fe3d2b0bec7a56ba36f0af689ce
troyjwingo <31901732+troyjwingo@users.noreply.github.com>
2017-11-14 20:42:41 -0600

    Delete Chalk.js

--- Chalk/Chalk.js
+++
@@ -1,5 +0,0 @@
-const chalk = require('chalk');
-
-var msg = 'Hello World';
-
-console.log(chalk.red('Hi Im Troy,'),chalk.green('I woke up this morning'),chalk.blue('Then I went back to bed.'),chalk.magenta('THE END.'));
